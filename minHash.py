b41fc1680e584a4d235d473ca21ac283506f79a6
Iris Lu <b05409028@ntu.edu>
2024-05-07 17:12:23 -0400

    2nd method for indexing

--- minHash.py
+++ minHash.py
@@ -8,20 +8,17 @@ import os
 
 # And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
-from pyspark.ml.linalg import Vectors
-from pyspark.sql.functions import collect_list
-from pyspark.sql import Row
-from pyspark.sql.functions import udf
 from pyspark.ml.linalg import Vectors, VectorUDT
+from pyspark.sql.functions import collect_list, udf, col
+from pyspark.sql import Row
+from pyspark.sql.types import IntegerType
 
-def to_sparse_vector(movie_ids, total_movies, unique_movie_ids):
+def to_sparse_vector(movie_ids, total_movies, movie_id_to_index):
     '''
     Notes
     1. indices need to start from 0
     2. indices need to be in a sorted order
     '''
-    # Create a dictionary for quick index lookup
-    movie_id_to_index = {movie_id: idx for idx, movie_id in enumerate(unique_movie_ids)}
 
     indices = sorted([movie_id_to_index[id]for id in movie_ids if id in movie_id_to_index])
     values = [1.0] * len(indices)
@@ -42,14 +39,28 @@ def main(spark, userID):
     
     # ratings_df = spark.read.csv("/Users/lvxinyuan/me/1-Projects/NYU/1-Courses/24_Spring_Big Data/hw/capstone-project-cap-19/ml-latest-small/ratings.csv")
 
-    # Group by userId and collect all movieIds into a list
-    ratings_df_grouped = ratings_df.groupBy("userId").agg(collect_list("movieId").alias("movieIds"))
-    # Show the transformed DataFrame
-    ratings_df_grouped.show()
-
     # Get all unique movieIds
     unique_movie_ids = ratings_df.select("movieId").distinct().rdd.flatMap(lambda x: x).collect()
     total_movies = len(unique_movie_ids) #9724
+
+    # Create a dictionary for quick index lookup
+    movie_id_to_index = {movie_id: idx for idx, movie_id in enumerate(unique_movie_ids)}
+
+    # Convert the dictionary into a DataFrame
+    movie_id_index_df = spark.createDataFrame(movie_id_to_index.items(), ["movieId", "index"])
+
+    # Join the original DataFrame with the index DataFrame
+    ratings_with_index_df = ratings_df.join(movie_id_index_df, on="movieId", how="left")
+
+    ratings_with_index_df.show()
+
+    
+    # Group by userId and collect all movieIds into a list
+    ratings_df_grouped = ratings_df.groupBy("userId").agg(collect_list("movieId").alias("movieIds"))
+    # Show the transformed DataFrame
+    # ratings_df_grouped.show()
+
+    
 
     # Create UDF with total_movies bound
     to_sparse_vector_udf = udf(lambda ids: to_sparse_vector(ids, total_movies), VectorUDT())
