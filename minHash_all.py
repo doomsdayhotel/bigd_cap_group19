ca6c8048c39383d3ba01f491daa235d08ee6e2c6
Iris Lu <b05409028@ntu.edu>
2024-05-09 16:38:34 -0400

    change reading file path

--- minHash_all.py
+++ minHash_all.py
@@ -74,7 +74,7 @@ def main(spark, userID):
         "JaccardDistance"
     )
     # Write the simplified DataFrame to CSV
-    simplified_df.write.csv('hdfs:/user/hl5679_nyu_edu/ml-latest-small/top_100_pairs2.csv', header=True, mode="overwrite")
+    simplified_df.write.csv('hdfs:/user/hl5679_nyu_edu/ml-latest/top_100_pairs_all', header=True, mode="overwrite")
 
     
 
@@ -90,47 +90,3 @@ if __name__ == "__main__":
 
     # Call our main routine
     main(spark, userID)
-
-
-''' Error message
-24/05/09 19:22:30 INFO org.sparkproject.jetty.server.AbstractConnector: Stopped Spark@71e6cb53{HTTP/1.1, (http/1.1)}{0.0.0.0:0}
-24/05/09 19:23:00 WARN org.apache.hadoop.util.ShutdownHookManager: ShutdownHook '$anon$2' timeout, java.util.concurrent.TimeoutException
-java.util.concurrent.TimeoutException
-        at java.util.concurrent.FutureTask.get(FutureTask.java:205)
-        at org.apache.hadoop.util.ShutdownHookManager.executeShutdown(ShutdownHookManager.java:124)
-        at org.apache.hadoop.util.ShutdownHookManager$1.run(ShutdownHookManager.java:95)
-24/05/09 19:23:00 WARN org.apache.spark.SparkContext: Ignoring Exception while stopping SparkContext from shutdown hook
-java.lang.InterruptedException
-        at java.lang.Object.wait(Native Method)
-        at java.lang.Thread.join(Thread.java:1257)
-        at java.lang.Thread.join(Thread.java:1331)
-        at org.apache.spark.scheduler.AsyncEventQueue.stop(AsyncEventQueue.scala:148)
-        at org.apache.spark.scheduler.LiveListenerBus.$anonfun$stop$1(LiveListenerBus.scala:269)
-        at org.apache.spark.scheduler.LiveListenerBus.$anonfun$stop$1$adapted(LiveListenerBus.scala:269)
-        at scala.collection.Iterator.foreach(Iterator.scala:943)
-        at scala.collection.Iterator.foreach$(Iterator.scala:943)
-        at scala.collection.AbstractIterator.foreach(Iterator.scala:1431)
-        at scala.collection.IterableLike.foreach(IterableLike.scala:74)
-        at scala.collection.IterableLike.foreach$(IterableLike.scala:73)
-        at scala.collection.AbstractIterable.foreach(Iterable.scala:56)
-        at org.apache.spark.scheduler.LiveListenerBus.stop(LiveListenerBus.scala:269)
-        at org.apache.spark.SparkContext.$anonfun$stop$13(SparkContext.scala:2083)
-        at org.apache.spark.util.Utils$.tryLogNonFatalError(Utils.scala:1419)
-        at org.apache.spark.SparkContext.stop(SparkContext.scala:2082)
-        at org.apache.spark.SparkContext.$anonfun$new$37(SparkContext.scala:669)
-        at org.apache.spark.util.SparkShutdownHook.run(ShutdownHookManager.scala:214)
-        at org.apache.spark.util.SparkShutdownHookManager.$anonfun$runAll$2(ShutdownHookManager.scala:188)
-        at scala.runtime.java8.JFunction0$mcV$sp.apply(JFunction0$mcV$sp.java:23)
-        at org.apache.spark.util.Utils$.logUncaughtExceptions(Utils.scala:1996)
-        at org.apache.spark.util.SparkShutdownHookManager.$anonfun$runAll$1(ShutdownHookManager.scala:188)
-        at scala.runtime.java8.JFunction0$mcV$sp.apply(JFunction0$mcV$sp.java:23)
-        at scala.util.Try$.apply(Try.scala:213)
-        at org.apache.spark.util.SparkShutdownHookManager.runAll(ShutdownHookManager.scala:188)
-        at org.apache.spark.util.SparkShutdownHookManager$$anon$2.run(ShutdownHookManager.scala:178)
-        at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)
-        at java.util.concurrent.FutureTask.run(FutureTask.java:266)
-        at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
-        at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
-        at java.lang.Thread.run(Thread.java:750)
-
-'''
